9d73ef1c935522025f467dc87bc88373fe591948
BUKA-T14P-GEN2\cng19 <1061863124@qq.com>
2026-07-10 17:27:39 +0800

    打包配置

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,30 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'js/[name]-[hash].js',
+        chunkFileNames: 'js/[name]-[hash].js',
+        assetFileNames: assetInfo => {
+          const ext = assetInfo.name?.split('.').pop()
+          if (ext === 'css') return 'css/[name]-[hash][extname]'
+          return `${ext}/[name]-[hash][extname]`
+        },
+        manualChunks(id) {
+          if (!id.includes('node_modules')) return
+          if (id.includes('node_modules/vue') || id.includes('node_modules/vue-router')) return 'vendor-vue'
+          if (id.includes('node_modules/ant-design-vue') || id.includes('node_modules/@ant-design')) return 'vendor-antd'
+          if (id.includes('node_modules/@wangeditor')) return 'vendor-editor'
+          if (id.includes('node_modules/echarts')) return 'vendor-echarts'
+          if (id.includes('node_modules/frappe-gantt')) return 'vendor-gantt'
+          if (id.includes('node_modules/axios')) return 'vendor-request'
+          if (id.includes('node_modules/dayjs')) return 'vendor-dayjs'
+          return 'vendor'
+        },
+      },
+    },
+  },
   server: {
     proxy: {
       '/api': {
